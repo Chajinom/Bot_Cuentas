2b1e1092915b25b82e5d7bbc27ed42a74b61a545
Omar Chajin Sastoque <chajinom@gmail.com>
2024-09-08 19:40:22 -0500

    modificacion en la app para obtener registros en un log html

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
+import json
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+# Configuracion de la Base de Datos SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Modelo de la tabla log
+
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
+    texto = db.Column(db.TEXT)
+
+# Crear la tabla si no existe
+with app.app_context():
+    db.create_all()
+
+
 @app.route('/')
-def hola_mundo():
-    return render_template('holaflask.html')
+#def hola_mundo():
+#    return render_template('holaflask.html')
+
+def index():
+    # obtener todos los registros de la base de datos
+    registros = Log.query.all()
+    return render_template('index.html', registros=registros)
+
+mensajes_log = []
+#Funcion para agregar mensajes y guardar en la base de datos
+def agregar_mensajes_log(texto):
+    mensajes_log.append(texto)
+
+    #Nos sirve para guardar el mensaje en la base de datos.
+    nuevo_registro = Log(texto=texto)
+    db.session.add(nuevo_registro)
+    db.session.commit()
+
+#agregar_mensajes_log(json.dumps("Mensaje de Test 1"))
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
